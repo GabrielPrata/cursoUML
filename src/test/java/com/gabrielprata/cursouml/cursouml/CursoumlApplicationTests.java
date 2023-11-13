93bf850351fea34f7601a7838b8b1eb7c9788d45
Gabriel <gabrielsprata@hotmail.com>
2023-11-12 22:24:59 -0300

    Initial Commit

---
+++ src/test/java/com/gabrielprata/cursouml/cursouml/CursoumlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gabrielprata.cursouml.cursouml;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CursoumlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
